97f4f0ae0d482edc4a4240eaae0cbaa55ca99088
Juan Illanes <juan.illanes.gauto@gmail.com>
2013-07-15 23:10:35 -0300

    Devolución de Pasaje/Encomienda

--- Aplicacion Desktop/FrbaBus/Cancelar Viaje/CancelarPasajesEncomiendas.Designer.cs
+++ Aplicacion Desktop/FrbaBus/Cancelar Viaje/CancelarPasajesEncomiendas.Designer.cs
@@ -28,19 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelVoucher = new System.Windows.Forms.Label();
+            this.txtVoucher = new System.Windows.Forms.TextBox();
+            this.dgvPasajesEncomiendas = new System.Windows.Forms.DataGridView();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txtMotivo = new System.Windows.Forms.TextBox();
+            this.labelMotivo = new System.Windows.Forms.Label();
+            this.labelSeleccion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPasajesEncomiendas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // labelVoucher
+            // 
+            this.labelVoucher.AutoSize = true;
+            this.labelVoucher.Location = new System.Drawing.Point(13, 13);
+            this.labelVoucher.Name = "labelVoucher";
+            this.labelVoucher.Size = new System.Drawing.Size(76, 13);
+            this.labelVoucher.TabIndex = 0;
+            this.labelVoucher.Text = "N° de voucher";
+            // 
+            // txtVoucher
+            // 
+            this.txtVoucher.Location = new System.Drawing.Point(95, 10);
+            this.txtVoucher.Name = "txtVoucher";
+            this.txtVoucher.Size = new System.Drawing.Size(100, 20);
+            this.txtVoucher.TabIndex = 1;
+            // 
+            // dgvPasajesEncomiendas
+            // 
+            this.dgvPasajesEncomiendas.AllowUserToAddRows = false;
+            this.dgvPasajesEncomiendas.AllowUserToDeleteRows = false;
+            this.dgvPasajesEncomiendas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPasajesEncomiendas.Location = new System.Drawing.Point(16, 64);
+            this.dgvPasajesEncomiendas.Name = "dgvPasajesEncomiendas";
+            this.dgvPasajesEncomiendas.RowHeadersVisible = false;
+            this.dgvPasajesEncomiendas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPasajesEncomiendas.Size = new System.Drawing.Size(504, 145);
+            this.dgvPasajesEncomiendas.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(407, 35);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(377, 253);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(130, 54);
+            this.btnCancelar.TabIndex = 4;
+            this.btnCancelar.Text = "Cancelar Pasajes/Encomiendas seleccionados";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // txtMotivo
+            // 
+            this.txtMotivo.Location = new System.Drawing.Point(72, 253);
+            this.txtMotivo.Multiline = true;
+            this.txtMotivo.Name = "txtMotivo";
+            this.txtMotivo.Size = new System.Drawing.Size(276, 54);
+            this.txtMotivo.TabIndex = 5;
+            // 
+            // labelMotivo
+            // 
+            this.labelMotivo.AutoSize = true;
+            this.labelMotivo.Location = new System.Drawing.Point(16, 253);
+            this.labelMotivo.Name = "labelMotivo";
+            this.labelMotivo.Size = new System.Drawing.Size(39, 13);
+            this.labelMotivo.TabIndex = 6;
+            this.labelMotivo.Text = "Motivo";
+            // 
+            // labelSeleccion
+            // 
+            this.labelSeleccion.AutoSize = true;
+            this.labelSeleccion.Location = new System.Drawing.Point(16, 212);
+            this.labelSeleccion.Name = "labelSeleccion";
+            this.labelSeleccion.Size = new System.Drawing.Size(432, 13);
+            this.labelSeleccion.TabIndex = 7;
+            this.labelSeleccion.Text = "Seleccione los Pasajes/Encomiendas a cancelar (mantener CTRL para selección múlti" +
+                "ple)";
             // 
             // CancelarPasajesEncomiendas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(532, 319);
+            this.Controls.Add(this.labelSeleccion);
+            this.Controls.Add(this.labelMotivo);
+            this.Controls.Add(this.txtMotivo);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dgvPasajesEncomiendas);
+            this.Controls.Add(this.txtVoucher);
+            this.Controls.Add(this.labelVoucher);
             this.Name = "CancelarPasajesEncomiendas";
             this.Text = "Cancelación de Pasajes / Encomiendas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPasajesEncomiendas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelVoucher;
+        private System.Windows.Forms.TextBox txtVoucher;
+        private System.Windows.Forms.DataGridView dgvPasajesEncomiendas;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox txtMotivo;
+        private System.Windows.Forms.Label labelMotivo;
+        private System.Windows.Forms.Label labelSeleccion;
     }
 }
--- Aplicacion Desktop/FrbaBus/Cancelar Viaje/CancelarPasajesEncomiendas.cs
+++ Aplicacion Desktop/FrbaBus/Cancelar Viaje/CancelarPasajesEncomiendas.cs
@@ -6,14 +6,113 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using FrbaBus.Filtros;
+using System.Data.SqlClient;
 
 namespace FrbaBus.Cancelar_Viaje
 {
     public partial class CancelarPasajesEncomiendas : Form
     {
+        private ErrorProvider errorVoucher = new ErrorProvider();
+        private ErrorProvider errorMotivo = new ErrorProvider();
+
         public CancelarPasajesEncomiendas()
         {
             InitializeComponent();
+            dgvPasajesEncomiendas.Visible = false;
+            btnCancelar.Visible = false;
+            txtMotivo.Visible = false;
+            labelMotivo.Visible = false;
+            labelSeleccion.Visible = false;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            dgvPasajesEncomiendas.DataSource = null;
+            // validar
+            bool isValid = true;
+            long voucher;
+            if (!long.TryParse(txtVoucher.Text, out voucher))
+            {
+                isValid = false;
+                errorVoucher.SetError(txtVoucher, "N° de voucher inválido");
+            }
+            if (isValid)
+            {
+                List<SqlParameter> parametros = new List<SqlParameter>();
+                parametros.Add(new SqlParameter("@voucher", voucher));
+                parametros.Add(new SqlParameter("@fecha", Config.FechaSistema));
+                DataTable items = DAC.ExecuteReader(@"SELECT id_pasaje ID, codigo_pasaje CODIGO, pasajero PASAJERO, NULL PESO, monto MONTO FROM DEL_NAVAL.pasajes p, DEL_NAVAL.viajes
+                    WHERE voucher = @voucher AND fecha_salida > @fecha AND p.cancelado = 0 AND id_viaje = viaje
+                    UNION 
+                    SELECT id_encomienda, codigo_encomienda, NULL, peso, monto  FROM DEL_NAVAL.encomiendas e, DEL_NAVAL.viajes
+                    WHERE voucher = @voucher AND fecha_salida > @fecha AND e.cancelado = 0 AND id_viaje = viaje", parametros);
+                if (items.Rows.Count == 0)
+                {
+                    errorVoucher.SetError(txtVoucher, "Los pasajes/encomiendas correspondientes ya fueron cancelados, o su viaje realizado");
+                    dgvPasajesEncomiendas.Visible = false;
+                    btnCancelar.Visible = false;
+                    txtMotivo.Visible = false;
+                    labelMotivo.Visible = false;
+                    return;
+                }
+                dgvPasajesEncomiendas.Visible = true;
+                btnCancelar.Visible = true;
+                txtMotivo.Visible = true;
+                labelMotivo.Visible = true;
+                labelSeleccion.Visible = true;
+
+                dgvPasajesEncomiendas.DataSource = items;
+            }
+            else
+            {
+                dgvPasajesEncomiendas.Visible = false;
+                btnCancelar.Visible = false;
+                txtMotivo.Visible = false;
+                labelMotivo.Visible = false;
+                labelSeleccion.Visible = false;
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            // validar
+            decimal monto = 0;
+            string codigoCancelacion = Aleatorio.Nuevo(20);
+            if (txtMotivo.Text != "")
+            {
+                foreach (DataGridViewRow item in dgvPasajesEncomiendas.SelectedRows)
+                {
+                    List<SqlParameter> parametros = new List<SqlParameter>();
+
+                    parametros.Add(new SqlParameter("@codigo_devolucion", codigoCancelacion));
+                    parametros.Add(new SqlParameter("@fecha", Config.FechaSistema));
+                    parametros.Add(new SqlParameter("@motivo", txtMotivo.Text));
+
+                    if (item.Cells["pasajero"].Value != DBNull.Value)
+                    {
+                        parametros.Add(new SqlParameter("@pasaje", int.Parse(item.Cells["id"].Value.ToString())));
+                        monto += Convert.ToDecimal(DAC.ExecuteScalar(@"set dateformat dmy
+                            declare @monto numeric(18,2)
+                            exec del_naval.devolverPasaje @pasaje, @codigo_devolucion, @fecha, @motivo, @monto output
+                            select @monto", parametros));
+                        
+                    }
+                    else
+                    {
+                        parametros.Add(new SqlParameter("@encomienda", int.Parse(item.Cells["id"].Value.ToString())));
+                        monto += Convert.ToDecimal(DAC.ExecuteScalar(@"set dateformat dmy
+                            declare @monto numeric(18,2)
+                            exec del_naval.devolverEncomienda @encomienda, @codigo_devolucion, @fecha, @motivo, @monto output
+                            select @monto", parametros));
+                    }
+                }
+                MessageBox.Show("Pasajes y/o encomiendas cancelados. Monto a devolver: " + monto.ToString());
+            }
+            else 
+            {
+                errorMotivo.SetError(txtMotivo, "Indique un motivo");
+            }
         }
     }
 }
